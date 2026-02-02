304e363e4f00b9003d5c9f2de4d466e4fec8e333
Jeffrey Gates <jeffreymrgates@gmail.com>
2026-02-02 16:41:47 -0500

    Wrote variables for lines 7-12, wrote it out to the console for line 17

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Jeff";
+            int age = 30;
+            double avg = 98.7;
+            decimal bankAccount = 123.33m;
+            char initial = 's';
+            bool isMarried = false;
+            
+            Console.WriteLine($"Hey, my name is {myName}\nAge: {age}: {avg}\n Money: {bankAccount}Initial: {initial}\n Married: {isMarried}");
         }
     }
 }
